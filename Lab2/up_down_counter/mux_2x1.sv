9c35a14a029641a72e42aa6344f5db709dfa4297
Andrew Wang <wangandrewwang88@gmail.com>
2024-04-22 13:51:31 -0700

    fixed

--- Lab2/up_down_counter/mux_2x1.sv
+++ Lab2/up_down_counter/mux_2x1.sv
@@ -1,10 +1,10 @@
 // 2to1 Multiplexor RTL code
 module mux_2x1 #(parameter WIDTH=4)  
 (
-  input logic in0, // Student to change in0 width to 4
-  input logic in1, // Student to change in1 width to 4
+  input logic [WIDTH-1:0] in0, // Student to change in0 width to 4
+  input logic [WIDTH-1:0] in1, // Student to change in1 width to 4
   input logic sel, 
-  output logic out // Student to change out width to 4
+  output logic [WIDTH-1:0] out // Student to change out width to 4
 ); 
   
   // always procedural block describing 2to1 Multiplexor behavior 
