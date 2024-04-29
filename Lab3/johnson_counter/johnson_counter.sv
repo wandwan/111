b20adcb7ae0007e22bcf12d51f0a3b94ae0560bd
Andrew Wang <wangandrewwang88@gmail.com>
2024-04-29 00:17:28 -0700

    fixed

---
+++ Lab3/johnson_counter/johnson_counter.sv
@@ -0,0 +1,19 @@
+// Johnson Counter RTL Model
+module johnson_counter (
+  input logic clk, clear, preset,
+  input logic[3:0] load_cnt,
+  output logic[3:0] count
+);
+always@(posedge clk or negedge clear) begin
+  // Student to add code for Johnson Counter  
+  if (!clear) begin
+    count <= 4'b0000;
+  end
+  else if (!preset) begin
+    count <= load_cnt;
+  end
+  else begin
+    count <= {count[2:0], count[3]};
+  end
+end
+endmodule: johnson_counter
