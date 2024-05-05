f05a1d108dd10fd511dba88437f688d2d18e483d
Andrew Wang <wangandrewwang88@gmail.com>
2024-05-05 02:19:51 -0700

    made simpler

--- Lab4/gray_code_to_binary_convertor/gray_code_to_binary_convertor.sv
+++ Lab4/gray_code_to_binary_convertor/gray_code_to_binary_convertor.sv
@@ -4,21 +4,23 @@ module gray_code_to_binary_convertor #(parameter N = 4)(
   output logic[N-1:0] binary_value);
   
   
-  logic [$clog2(N):0][N-1:0] tmps;
+function automatic [N-1:0] gray_to_binary(logic[N-1:0] gray_value);
+  begin
+    gray_to_binary = gray_value;
+    for(int i=N-2; i>=0; i=i-1) begin
+      gray_to_binary[i] = gray_to_binary[i] ^ gray_to_binary[i+1];
+    end
+  end
+endfunction
+
   // Add code for gray code to binary conversion
   always_ff @(posedge clk or negedge rstn) begin
     if(~rstn) begin
       binary_value <= 0;
     end
     else begin
-      binary_value[0] <= gray_value[0];
-      tmps[0] <= gray_value ^ (gray_value >> 1);
-      for(int i=1; i<$clog2(N); i=i+1) begin
-        tmps[i] <= tmps[i-1] ^ (tmps[i-1] >> (1 << i));
+      binary_value <= gray_to_binary(gray_value);
     end
   end
-  end
-  
-  assign binary_value = tmps[$clog2(N)-1];
 
 endmodule: gray_code_to_binary_convertor
