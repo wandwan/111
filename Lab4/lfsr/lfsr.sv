f63622cec02583db160b5dc6a41f8e3df98648dd
Andrew Wang <wangandrewwang88@gmail.com>
2024-05-05 14:20:58 -0700

    fixed

--- Lab4/lfsr/lfsr.sv
+++ Lab4/lfsr/lfsr.sv
@@ -37,7 +37,7 @@ always_ff @(posedge clk, negedge reset) begin
     // $display("count=%d, lfsr_reg=%b", count, lfsr_reg);
   end
 end
-assign lfsr_done = count == ((1 << N) - 1);
+assign lfsr_done = count == ((1 << N) - 2);
 assign lfsr_data = lfsr_reg;
  
 endmodule: lfsr
