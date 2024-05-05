d255b4a6718260f0e7df093cf7a01e5600c3f9e2
Andrew Wang <wangandrewwang88@gmail.com>
2024-05-05 14:30:31 -0700

    fixed bs

--- Lab4/barrel_shifter/barrel_shifter.sv
+++ Lab4/barrel_shifter/barrel_shifter.sv
@@ -21,19 +21,19 @@ end
 genvar i;
 generate
   for(i=0; i<4; i=i+1) begin: left_shift
-    mux_2x1 l0 mux_2x1_inst(
+    mux_2x1 mux_2x1_inst(
       .in0(left_shifted[0][i]),
       .in1(left_shifted[1][i]),
       .sel(select),
       .out(mux_out[1][i])
     );
-    mux_2x1 r0 mux_2x1_inst(
+    mux_2x1 mux_2x1_inst(
       .in0(right_shifted[0][i]),
       .in1(right_shifted[1][i]),
       .sel(select),
       .out(mux_out[0][i])
     );
-    mux_2x1 m0 mux_2x1_inst(
+    mux_2x1 mux_2x1_inst(
       .in0(mux_out[0][i]),
       .in1(mux_out[1][i]),
       .sel(direction),
